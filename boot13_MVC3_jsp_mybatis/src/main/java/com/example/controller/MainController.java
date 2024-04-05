0287ce26617a6837c8ab23efdfc0e7b251391e50
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-05 12:40:49 +0900

    third commit

--- boot13_MVC3_jsp_mybatis/src/main/java/com/example/controller/MainController.java
+++ boot13_MVC3_jsp_mybatis/src/main/java/com/example/controller/MainController.java
@@ -25,6 +25,7 @@ public class MainController {
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String main() {
 		System.out.println("///////////////////////");
+		System.out.println("젠킨스");
 		return "main"; //main.jsp 
 	}
 
